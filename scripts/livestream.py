49dc42a1ed8250a475637435bf9d952f5f5bf1ed
Siddharth <sidinfinity0@gmail.com>
2021-06-30 13:05:08 -0700

    changed image location

--- scripts/livestream.py
+++ scripts/livestream.py
@@ -7,6 +7,9 @@ import time
 from json import loads
 from PIL import Image as im
 
+cap = cv2.VideoCapture(0)
+imageSave = '../../Desktop/image.jpg'
+
 def drawBox(bBoxes):
     for box in bBoxes:
         if box['score'] > 50.0:
@@ -15,22 +18,28 @@ def drawBox(bBoxes):
 def getResults():
     api_key = 'WbeirrLIJJ696lyWin1TAw6z25NxS4Mq'
     url = 'https://app.nanonets.com/api/v2/ObjectDetection/Model/49fb94bf-128b-4a8b-8242-dbf2d7f06f65/LabelFile/'
-    data = {'file': open('./image.jpg', 'rb')}
+    data = {'file': open(imageSave, 'rb')}
     response = requests.post(url, auth=requests.auth.HTTPBasicAuth(api_key, ''), files=data)
     
     return loads(response.text)
 
 def takePhoto(frame):
+    data = im.fromarray(frame)
+    data.save(imageSave)
     bBoxes = getResults()['result'][0]['prediction']
+    print(getResults()['result'])
+
     for box in bBoxes:
         frame = cv2.rectangle(frame, (box['xmin'], box['ymin']), (box['xmax'], box['ymax']), (225, 0, 0), 5)
         
     data = im.fromarray(frame)
-    data.save("image.jpg")
-    print('Success')
+    data.save(imageSave)
+    print('success')
+    print(bBoxes)
+    return
 
-cap = cv2.VideoCapture(0)
-previousFrame = np.ndarray(shape=(480, 640, 3), dtype=int)
+
+
 while True:
     _temp, frame = cap.read()
     
@@ -39,11 +48,12 @@ while True:
     
     if cv2.waitKey(1) & 0xFF == ord('p'):
         takePhoto(frame)
-        
+    
+    time.sleep(0.1)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
-    time.sleep(0.1)
     
 
 
